e33e4238370581b83718ddd3580e5c4c51467394
Serg M <msbox16@gmail.com>
2023-08-15 13:00:54 +0100

    nc-news-3

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const app = express();
 const { getAllTopics } = require('./controllers/topic.controller');
+const endpoints = require('./endpoints.json');
+
+app.use(express.json());
+
+app.get('/api', (req, res) => {
+	res.json(endpoints);
+});
 
 app.get('/api/topics', getAllTopics);
+
+
+
+
 
 
 module.exports = app;
