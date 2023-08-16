f73cedbdc6f80b5bc8018f334bcdd7d390af3b45
Serg M <msbox16@gmail.com>
2023-08-16 13:16:11 +0100

    nc-news-3 with changes

--- app.js
+++ app.js
@@ -10,5 +10,18 @@ app.get('/api', getApiDocumentation);
 app.get('/api/topics', getAllTopics);
 app.get('/api/articles/:article_id', getArticleById);
 
+app.use((err, req, res, next) => {
+	console.log('Error: ', err);
+
+	if (err.message === 'Article not found') {
+		res.status(404).send({ message: err.message });
+	} else if (err.code === '22P02') {
+		res.status(400).send({ message: 'Invalid article_id' });
+	} else {
+		res.status(500).send({ message: 'Internal Server Error' });
+	}
+})
+
+
 
 module.exports = app;
--- models/article.model.js
+++ models/article.model.js
@@ -2,10 +2,10 @@ const db = require('../db/connection');
 
 exports.selectArticleById = (article_id) => {
 	return db
-		.query('SELECT * FROM articles WHERE article_id=$1;', [article_id])
+		.query('SELECT * FROM articles WHERE article_id = $1;', [article_id])
 		.then((article) => {
 			if (article.rows.length === 0) {
-				return Promise.reject(new Error(`Article not found`));
+				return Promise.reject(new Error('Article not found'));
 			}
 			return article.rows[0];
 		})
