3b2c5245338dc8360afbe3cd7ef839e736526368
Serg M <msbox16@gmail.com>
2023-08-18 10:51:31 +0100

    nc-new-7

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const express = require('express');
-const app = express();
+
 const { getAllTopics } = require('./controllers/topic.controller');
 const { getApiDocumentation } = require('./controllers/api.controller');
 const { getArticleById, getAllArticles } = require('./controllers/article.controller');
-const { getCommentsByArticleId } = require('./controllers/comment.controller');
+const { getCommentsByArticleId, postCommentByArticleId } = require('./controllers/comment.controller');
 
+const app = express();
+app.use(express.json());
 
 app.get('/api', getApiDocumentation);
 app.get('/api/topics', getAllTopics);
 app.get('/api/articles/:article_id', getArticleById);
 app.get('/api/articles', getAllArticles);
 app.get('/api/articles/:article_id/comments', getCommentsByArticleId);
+app.post('/api/articles/:article_id/comments', postCommentByArticleId);
 
 app.use((err, req, res, next) => {
 	if (err.message === 'Article not found') {
--- controllers/comment.controller.js
+++ controllers/comment.controller.js
@@ -1,4 +1,4 @@
-const { selectCommentsByArticleId } = require('../models/comment.model');
+const { selectCommentsByArticleId, insertComment } = require('../models/comment.model');
 const { selectArticleById } = require('../models/article.model');
 
 
@@ -14,4 +14,27 @@ exports.getCommentsByArticleId = (req, res, next) => {
 			res.status(200).send({ comments });
 		})
 		.catch(next)
+}
+exports.postCommentByArticleId = (req, res, next) => {
+	const { article_id } = req.params;
+	const { username, body } = req.body;
+
+	if (!username || !body) {
+		return res.status(400).send({ message: 'Bad Request' })
+	}
+
+	selectArticleById(article_id)
+		.then((article) => {
+			if (!article) {
+				req.status(404).send({ message: err.message });
+			} else {
+				insertComment(article_id, username, body)
+					.then((comment) => {
+						res.status(201).send({ comment })
+					})
+					.catch(next);
+			}
+		})
+		.catch(next)
+
 }
